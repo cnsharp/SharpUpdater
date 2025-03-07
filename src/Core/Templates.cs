5143f2e8b9f737fe588ebbb73e08424f588b3f03
Bob Chin <cnsharp@yeah.net>
2025-03-07 21:09:41 +0800

    Remove placeholder of AppName

--- src/Core/Templates.cs
+++ src/Core/Templates.cs
@@ -5,7 +5,7 @@
         public const string ManifestXml =
             @"<Manifest xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">
   <Id>$id$</Id>
-  <AppName>$id$</AppName>
+  <AppName></AppName>
   <Owner>$owner$</Owner>
   <Description><![CDATA[$description$]]></Description>
   <Copyright>$copyright$</Copyright>
